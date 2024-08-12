ee43d0cd019a27e243b4a9166dc2311a6432a338
Khalilullah Mohman <kmohman2@gmail.com>
2024-08-12 11:46:13 -0400

    double quotes added

---
+++ app/api/signin.js
@@ -0,0 +1,21 @@
+// app/signin/page.js
+'use client';
+import { useState } from 'react';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Add API call to sign in endpoint here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
